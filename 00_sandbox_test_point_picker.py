aea75dda77af700b9a7bd6ec63f2cbec2dee7a97
Abel Pacheco Ortega <dougbel.unam@gmail.com>
2021-05-13 16:54:03 +0100

    Enhance: Best test point picker
    we

--- 00_sandbox_test_point_picker.py
+++ 00_sandbox_test_point_picker.py
@@ -25,7 +25,7 @@ import trimesh
 import vedo.utils
 
 from util.util_interactive import Selector
-from util.util_mesh import read_full_mesh_sdf
+from util.util_mesh import read_full_mesh_sdf, define_scene_boundary_on_the_fly
 from util.util_preprocessing import crop_scene_cube_smplx_at_point
 from utils_read_data import define_scene_boundary
 
@@ -106,53 +106,33 @@ print('[INFO] vposer model loaded')
 
 # In[3]:
 
-rot_angle_1, scene_min_x, scene_max_x, scene_min_y, scene_max_y = define_scene_boundary('prox', scene_name)
-# rot_angle_1t, scene_min_xt, scene_max_xt, scene_min_yt, scene_max_yt =define_scene_boundary_on_the_fly(scene_trimesh)
+# rot_angle_1, scene_min_x, scene_max_x, scene_min_y, scene_max_y = define_scene_boundary('prox', scene_name)
+rot_angle_1, scene_min_x, scene_max_x, scene_min_y, scene_max_y =define_scene_boundary_on_the_fly(scene_trimesh)
+print('[INFO] calculated a)rotation to parallel, scene boundary .')
 
 
 scene_verts = rotate_scene_smplx_predefine(cur_scene_verts, rot_angle=rot_angle_1)
+print('[INFO] rotated scene mesh to parallel.')
 
 rotated_scene = trimesh.Trimesh(vertices=scene_verts, faces=scene_trimesh.faces)
 rotated_scene.visual.face_colors = scene_trimesh.visual.face_colors
 
 
-# scene_verts_local, scene_verts_crop_local, shift = crop_scene_cube_smplx_predifine(
-#     scene_verts, r=cube_size, with_wall_ceilling=True, random_seed=np.random.randint(10000),
-#     scene_min_x=scene_min_x, scene_max_x=scene_max_x, scene_min_y=scene_min_y, scene_max_y=scene_max_y,
-#     rotate=True)
 sel_gui = Selector(rotated_scene, scene_min_x, scene_max_x, scene_min_y, scene_max_y)
 selected_p = sel_gui.select_point_to_test()
+
+print('[INFO] Position selected.')
+
+ROTATE_CUBE = True
+
+
 scene_verts_local, scene_verts_crop_local, shift = crop_scene_cube_smplx_at_point(
      scene_verts, scene_center=selected_p, r=cube_size, with_wall_ceilling=True, random_seed=np.random.randint(10000),
      scene_min_x=scene_min_x, scene_max_x=scene_max_x, scene_min_y=scene_min_y, scene_max_y=scene_max_y,
-     rotate=True)
+     rotate=ROTATE_CUBE)
 
 
 print('[INFO] scene mesh cropped and shifted.')
-
-#define the cube to compare
-# orig_scene = scene_trimesh.copy(include_cache=True)
-
-shifted_rotated_scene = trimesh.Trimesh(vertices=scene_verts_local, faces=scene_trimesh.faces)
-shifted_rotated_scene.visual.face_colors = scene_trimesh.visual.face_colors
-
-# s = trimesh.Scene()
-# s.add_geometry(orig_scene)
-# # s.show(caption=scene_name+" original scene", flags={'axis': 'world'})
-# orig_scene.visual.face_colors = [255, 0, 0, 20]
-# s.add_geometry(rotated_scene)
-# # s.show(caption=scene_name+"parallel scene", flags={'axis': 'world'})
-# rotated_scene.visual.face_colors = [0, 255, 0, 20]
-# s.add_geometry(shifted_rotated_scene)
-# # s.show(caption=scene_name, flags={'axis': 'world'})
-# s.add_geometry(trimesh.points.PointCloud(scene_verts_crop_local, colors="yellow"))
-# s.show(caption=scene_name, flags={'axis': 'world'})
-
-vp=vedo.Plotter(bg="white", axes=2)
-vp.show([vedo.Spheres(scene_verts_crop_local, r=.007, c="yellow", alpha=1).lighting("plastic"),
-         vedo.utils.trimesh2vtk(shifted_rotated_scene).lighting('ambient')])
-#ambient=0.8, diffuse=0.2, specular=0.1, specularPower=1, specularColor=(1,1,1)
-
 
 scene_basis_set = bps_gen_ball_inside(n_bps=10000, random_seed=100)
 scene_verts_global, scene_verts_crop_global, rot_angle_2 =     augmentation_crop_scene_smplx(scene_verts_local / cube_size,
@@ -196,18 +176,54 @@ scene_AE.eval()
 body_dec.eval()
 
 
+print('[INFO] pretrained weights loaded.')
+
+
+SHOW_OUTPUTS = True
+
+shifted_rotated_scene = trimesh.Trimesh(vertices=scene_verts_local, faces=scene_trimesh.faces)
+shifted_rotated_scene.visual.face_colors = scene_trimesh.visual.face_colors
+
+unselected_scene_verts_local = [scene_verts_crop_local[i] for i in range(len(scene_verts_crop_local))  if i not in selected_ind]
+# vp=vedo.Plotter(bg="white", axes=2)
+# vp.show([vedo.Spheres(selected_scene_verts_local, r=.007, c="green", alpha=1).lighting("plastic"),
+#          vedo.Spheres(unselected_scene_verts_local, r=.002, c="yellow", alpha=1).lighting("plastic"),
+#                   vedo.utils.trimesh2vtk(shifted_rotated_scene).lighting('ambient')])
+#ambient=0.8, diffuse=0.2, specular=0.1, specularPower=1, specularColor=(1,1,1)
+
+s = trimesh.Scene()
+s.add_geometry(shifted_rotated_scene)
+s.add_geometry(trimesh.points.PointCloud(selected_scene_verts_local, colors=[0,255,0]))
+s.add_geometry(trimesh.points.PointCloud(unselected_scene_verts_local, colors=[255,255,0]))
+
+s.show(caption=scene_name)
+
+
+
 ######################## random sample a body  ##########################
 scene_bps_verts = scene_bps_verts / cube_size
 
 _, scene_bps_feat = scene_bps_AE(scene_bps)
 _, scene_bps_verts_feat = scene_AE(scene_bps_verts)
-body_bps_sample = c_VAE.sample(1, scene_bps_feat)  # [1, 1, 10000]
-body_verts_sample, body_shift = body_dec(body_bps_sample,
-                                         scene_bps_verts_feat)  # [1, 3, 10475], unit ball scale, local coordinate
-body_shift = body_shift.repeat(1, 1, 10475).reshape([body_verts_sample.shape[0], 10475, 3])  # [bs, 10475, 3]
-body_verts_sample = body_verts_sample + body_shift.permute(0, 2, 1)  # [bs=1, 3, 10475]
+
+# [1, 1, 10000]
+body_bps_sample = c_VAE.sample(1, scene_bps_feat)
+# [1, 3, 10475], unit ball scale, local coordinate
+body_verts_sample, body_shift = body_dec(body_bps_sample, scene_bps_verts_feat)
+# [bs, 10475, 3]
+body_shift = body_shift.repeat(1, 1, 10475).reshape([body_verts_sample.shape[0], 10475, 3])
+# [bs=1, 3, 10475]
+body_verts_sample = body_verts_sample + body_shift.permute(0, 2, 1)
+
 print('[INFO] a random body is generated.')
 
+
+body_trimesh_sampled = trimesh.Trimesh(vertices=body_verts_sample.detach().cpu().numpy().squeeze().transpose()*cube_size, faces=smplx_model.faces, face_colors=[200, 200, 200, 255])
+
+s = trimesh.Scene()
+s.add_geometry(shifted_rotated_scene)
+s.add_geometry(body_trimesh_sampled)
+s.show(caption=scene_name)
 
 # ### 5. Optimization stage 1: perform simple optimization (without interaction-based losses)
 
@@ -261,7 +277,7 @@ for step in tqdm(range(itr_s1)):
         nbrs = NearestNeighbors(n_neighbors=1, leaf_size=1, algorithm="ball_tree").fit(
             body_verts_rec_global.detach().cpu().numpy())
         neigh_dist, neigh_ind = nbrs.kneighbors(selected_scene_verts_global)
-        body_bps_rec = body_verts_rec_global[neigh_ind.squeeze()] -                         torch.from_numpy(selected_scene_verts_global).float().to(device)  # [n_bps, 3]
+        body_bps_rec = body_verts_rec_global[neigh_ind.squeeze()] - torch.from_numpy(selected_scene_verts_global).float().to(device)  # [n_bps, 3]
         body_bps_rec = torch.sqrt(
             body_bps_rec[:, 0] ** 2 + body_bps_rec[:, 1] ** 2 + body_bps_rec[:, 2] ** 2).unsqueeze(0).unsqueeze(0)  # [bs=1, 1, n_bps]
 
@@ -280,12 +296,23 @@ for step in tqdm(range(itr_s1)):
     hand_params = body_params_rec_72[:, 48:]
     loss_hand = torch.mean(hand_params ** 2)
 
-    loss = weight_loss_rec_verts * loss_rec_verts + weight_loss_rec_bps * loss_rec_bps +            weight_loss_vposer * loss_vposer +            weight_loss_shape * loss_shape +            weight_loss_hand * loss_hand
+    loss = weight_loss_rec_verts * loss_rec_verts + weight_loss_rec_bps * loss_rec_bps + weight_loss_vposer * loss_vposer + weight_loss_shape * loss_shape + weight_loss_hand * loss_hand
     loss.backward(retain_graph=True)
     optimizer.step()
 
 print('[INFO] optimization stage 1 finished.')
 
+body_params_opt_s1 = convert_to_3D_rot(body_params_rec)  # tensor, [bs=1, 72]
+body_pose_joint_s1 = vposer_model.decode(body_params_opt_s1[:, 16:48], output_type='aa').view(1, -1)
+body_verts_opt_s1 = gen_body_mesh(body_params_opt_s1, body_pose_joint_s1, smplx_model)[0]
+body_verts_opt_s1 = body_verts_opt_s1.detach().cpu().numpy()
+
+body_trimesh_s1 = trimesh.Trimesh(vertices=body_verts_opt_s1, faces=smplx_model.faces, face_colors=[200, 200, 200, 255])
+
+s = trimesh.Scene()
+s.add_geometry(shifted_rotated_scene)
+s.add_geometry(body_trimesh_s1)
+s.show(caption=scene_name)
 
 # ### 6. Optimization stage 2: perform advanced optimizatioin (interaction-based), with contact and collision loss
 
@@ -318,7 +345,7 @@ for step in tqdm(range(itr_s2)):
         nbrs = NearestNeighbors(n_neighbors=1, leaf_size=1, algorithm="ball_tree").fit(
             body_verts_rec_global.detach().cpu().numpy())
         neigh_dist, neigh_ind = nbrs.kneighbors(selected_scene_verts_global)
-        body_bps_rec = body_verts_rec_global[neigh_ind.squeeze()] -                        torch.from_numpy(selected_scene_verts_global).float().to(device)  # [n_bps, 3]
+        body_bps_rec = body_verts_rec_global[neigh_ind.squeeze()] - torch.from_numpy(selected_scene_verts_global).float().to(device)  # [n_bps, 3]
         body_bps_rec = torch.sqrt(
             body_bps_rec[:, 0] ** 2 + body_bps_rec[:, 1] ** 2 + body_bps_rec[:, 2] ** 2).unsqueeze(0).unsqueeze(0)  # [bs=1, 1, n_bps]
 
@@ -365,7 +392,7 @@ for step in tqdm(range(itr_s2)):
     contact_dist, _ = dist_chamfer_contact(body_verts_contact.contiguous(),scene_verts.contiguous())
     loss_contact = torch.mean(torch.sqrt(contact_dist + 1e-4) / (torch.sqrt(contact_dist + 1e-4) + 1.0))
 
-    loss = weight_loss_rec_verts * loss_rec_verts + weight_loss_rec_bps * loss_rec_bps +            weight_loss_vposer * loss_vposer +            weight_loss_shape * loss_shape +            weight_loss_hand * loss_hand +            weight_collision * loss_collision + weight_loss_contact * loss_contact
+    loss = weight_loss_rec_verts * loss_rec_verts + weight_loss_rec_bps * loss_rec_bps + weight_loss_vposer * loss_vposer + weight_loss_shape * loss_shape + weight_loss_hand * loss_hand + weight_collision * loss_collision + weight_loss_contact * loss_contact
     loss.backward(retain_graph=True)
     optimizer.step()
 
@@ -383,6 +410,15 @@ body_pose_joint = vposer_model.decode(body_params_opt_s2[:, 16:48], output_type=
 body_verts_opt_s2 = gen_body_mesh(body_params_opt_s2, body_pose_joint, smplx_model)[0]
 body_verts_opt_s2 = body_verts_opt_s2.detach().cpu().numpy()   # [n_body_vert, 3]
 
+body_trimesh_s2 = trimesh.Trimesh(vertices=body_verts_opt_s2, faces=smplx_model.faces, face_colors=[200, 200, 200, 255])
+
+s = trimesh.Scene()
+s.add_geometry(shifted_rotated_scene)
+s.add_geometry(body_trimesh_s2)
+s.show(caption=scene_name)
+
+
+
 # transfrom the body verts to the PROX world coordinate system
 body_verts_opt_prox_s2 = np.zeros(body_verts_opt_s2.shape)  # [10475, 3]
 temp = body_verts_opt_s2 - shift
@@ -390,14 +426,6 @@ body_verts_opt_prox_s2[:, 0] = temp[:, 0] * math.cos(-rot_angle_1) - temp[:, 1] 
 body_verts_opt_prox_s2[:, 1] = temp[:, 0] * math.sin(-rot_angle_1) + temp[:, 1] * math.cos(-rot_angle_1)
 body_verts_opt_prox_s2[:, 2] = temp[:, 2]
 
-
-# body_mesh_opt_s2 = o3d.geometry.TriangleMesh()
-# body_mesh_opt_s2.vertices = o3d.utility.Vector3dVector(body_verts_opt_prox_s2)
-# body_mesh_opt_s2.triangles = o3d.utility.Vector3iVector(smplx_model.faces)
-# body_mesh_opt_s2.compute_vertex_normals()
-
-# use normal open3d visualization
-# o3d.visualization.draw_geometries([scene_trimesh, body_mesh_opt_s2])
 
 body_trimesh_opt_s2 = trimesh.Trimesh(vertices=body_verts_opt_prox_s2, faces=smplx_model.faces, face_colors=[200, 200, 200, 255])
 
@@ -407,24 +435,4 @@ s.add_geometry(scene_trimesh)
 s.add_geometry(body_trimesh_opt_s2)
 body_trimesh_opt_s2.export(f"body{np.random.randint(10000)}.ply")
 s.show(caption=scene_name)
-
-#  # use webGL
-# visualizer = JVisualizer() 
-# # create pointcloud
-# pcd_scene = o3d.geometry.PointCloud()
-# pcd_scene.points = scene_mesh.vertices
-# pcd_scene.colors = scene_mesh.vertex_colors
-
-# pcd_body = o3d.geometry.PointCloud()
-# pcd_body.points = body_mesh_opt_s2.vertices
-
-# visualizer.add_geometry(pcd_scene)
-# visualizer.add_geometry(pcd_body)
-# visualizer.show()
-
-
-# In[ ]:
-
-
-
 
