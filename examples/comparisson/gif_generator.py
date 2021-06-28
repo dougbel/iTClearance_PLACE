a12ef7a4aa6f9dd791e13203a302e66c6667a69c
Abel Pacheco Ortega <dougbel.unam@gmail.com>
2021-06-28 17:55:26 +0100

    Enhance: Better config on gif generation

--- examples/comparisson/gif_generator.py
+++ examples/comparisson/gif_generator.py
@@ -21,30 +21,34 @@ def get_files_with_extension(directory, extension):
     return [file_name for file_name in os.listdir(directory) if file_name.endswith(extension)]
 
 
-def generate_gif(trimesh_env, trimesh_body,view_center,  save_on_file=None):
-
+def generate_gif(trimesh_env, trimesh_body, view_center,  save_on_file=None):
 
     record=True if save_on_file is not None else False
 
 
     env_mesh = Mesh.from_trimesh(trimesh_env)
 
-    trimesh_body.visual.face_colors = [0, 255, 0, 255]
+    trimesh_body.visual.face_colors = [10, 220, 10, 255]
     body_mesh = Mesh.from_trimesh(trimesh_body, smooth=False)
 
-    trimesh_body.visual.face_colors =  [100, 100, 100, 100]
+    trimesh_body.visual.face_colors =  [40, 40, 40, 150]
     body_mesh_w = Mesh.from_trimesh(trimesh_body, smooth=False, wireframe=True)
 
+    # reference_mesh = Mesh.from_trimesh(trimesh.primitives.Sphere(center = np_point), smooth=False, wireframe=True)
+
     scene = Scene(ambient_light=np.array([0.5, 0.5, 0.5]))
-    light = DirectionalLight(color=[1.0, 1.0, 1.0], intensity=2.0)
+    # light = DirectionalLight(color=[.80, .80, .80], intensity=1.0)
     scene.add(env_mesh)
     scene.add(body_mesh)
     scene.add(body_mesh_w)
-    scene.add(light)
+    # scene.add(reference_mesh)
+    # scene.add(light)
     v = Viewer(scene,
                run_in_thread=True,
                render_flags={"shadows": True},
-               viewer_flags={"rotate": True, "rotate_axis": [0, 0, 1], "view_center": view_center,
+               viewer_flags={"rotate": True,
+                             "rotate_axis": [0, 0, 1],
+                             "view_center": view_center,
                              "rotate_rate": np.pi / 4.0,
                              "record": record,
                              "use_perspective_cam": True
@@ -56,7 +60,6 @@ def generate_gif(trimesh_env, trimesh_body,view_center,  save_on_file=None):
     # for "use_perspective_cam": False
     # dy=.25
     # spfc = 0.95
-    # spbc = 1.0 / 0.95
     # sf = 1.0
     # sf = spfc * dy
     # c = v._camera_node.camera
@@ -85,6 +88,8 @@ if __name__ == '__main__':
     directory_datasets = opj(base_dir, "datasets")
 
     samples_it_dir = opj(base_dir, "test", "sampled_it_clearance")
+    samples_it_opti_down_dir = opj(base_dir, "test", "sampled_it_clearance_opti_down_trans")
+
     samples_place_dir = opj(base_dir, "test", "sampled_place_exec")
 
     output_dir = opj(base_dir, 'test', 'gifted_place_auto_samples_extracted')
@@ -110,12 +115,14 @@ if __name__ == '__main__':
         random.shuffle(pending_tasks)
 
     for dataset_name, env_name, interaction in pending_tasks:
+
+        # if not( env_name == "zsNo4HB9uLZ-bedroom0_0" and interaction == "standing_up"):
+        #      continue
+
         print( dataset_name, env_name, interaction)
         it_subdir = opj(samples_it_dir, env_name, interaction)
+        it_opti_down_subdir = opj(samples_it_opti_down_dir, env_name, interaction)
         place_subdir = opj(samples_place_dir, env_name, interaction)
-        output_subdir = opj(output_dir, env_name, interaction)
-        if not os.path.exists(output_subdir):
-            os.makedirs(output_subdir)
 
         for np_point_file_name in get_files_with_extension(it_subdir, ".npy"):
             print(np_point_file_name)
@@ -125,15 +132,33 @@ if __name__ == '__main__':
             file_mesh_env = opj(directory_datasets, dataset_name, "scenes", env_name + ".ply")
             trimesh_env = trimesh.load(file_mesh_env)
 
-            place_b0_orig = trimesh.load(opj(place_subdir, f"body_{n}_orig.ply"))
-            generate_gif(trimesh_env, place_b0_orig, np_point, opj(output_subdir,f"body_{n}_orig.gif"))
-            place_b0_orig = trimesh.load(opj(place_subdir, f"body_{n}_opt1.ply"))
-            generate_gif(trimesh_env, place_b0_orig, np_point, opj(output_subdir,f"body_{n}_opt1.gif"))
-            place_b0_orig = trimesh.load(opj(place_subdir, f"body_{n}_opt2.ply"))
-            generate_gif(trimesh_env, place_b0_orig, np_point, opj(output_subdir,f"body_{n}_opt2.gif"))
+            #calculating view center
+            view_center = np.copy(np_point)
+            if view_center[2] < trimesh_env.vertices.min(axis=0)[2] + .5:
+                view_center[2] = trimesh_env.vertices.min(axis=0)[2] + .5
+            elif view_center[2] > trimesh_env.vertices.min(axis=0)[2] + 1:
+                view_center[2] = trimesh_env.vertices.min(axis=0)[2] + 1
 
-            place_b0_orig = trimesh.load(opj(it_subdir, f"body_{n}.ply"))
-            generate_gif(trimesh_env, place_b0_orig, np_point, opj(output_subdir,f"body_{n}.gif"))
+            output_subdir = opj(output_dir, env_name, interaction, "place")
+            if not os.path.exists(output_subdir):
+                os.makedirs(output_subdir)
+
+            trimesh_body = trimesh.load(opj(place_subdir, f"body_{n}_orig.ply"))
+            generate_gif(trimesh_env, trimesh_body, view_center, opj(output_subdir, f"body_{n}_orig.gif"))
+            trimesh_body = trimesh.load(opj(place_subdir, f"body_{n}_opt1.ply"))
+            generate_gif(trimesh_env, trimesh_body, view_center, opj(output_subdir, f"body_{n}_opt1.gif"))
+            trimesh_body = trimesh.load(opj(place_subdir, f"body_{n}_opt2.ply"))
+            generate_gif(trimesh_env, trimesh_body, view_center, opj(output_subdir, f"body_{n}_opt2.gif"))
+
+            output_subdir = opj(output_dir, env_name, interaction, "it")
+            if not os.path.exists(output_subdir):
+                os.makedirs(output_subdir)
+
+            trimesh_body = trimesh.load(opj(it_subdir, f"body_{n}.ply"))
+            generate_gif(trimesh_env, trimesh_body, view_center, opj(output_subdir, f"body_{n}.gif"))
+            trimesh_body = trimesh.load(opj(it_opti_down_subdir, f"body_{n}.ply"))
+            generate_gif(trimesh_env, trimesh_body, view_center, opj(output_subdir, f"body_{n}_opti_down.gif"))
+
 
         if save_results:
             num_completed_task += 1
