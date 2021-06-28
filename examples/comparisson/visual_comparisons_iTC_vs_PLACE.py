70d7876ed4d108789d54103bf5c9023afab476e0
Abel Pacheco Ortega <dougbel.unam@gmail.com>
2021-06-28 14:21:21 +0100

    Enhance: Visualisation naive it optimisation

--- examples/comparisson/visual_comparisons_iTC_vs_PLACE.py
+++ examples/comparisson/visual_comparisons_iTC_vs_PLACE.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     dataset_path = opj(base_dir, "datasets")
 
     samples_it_dir = opj(base_dir, "test", "sampled_it_clearance")
+    samples_it_optim_down_dir = opj(base_dir, "test", "sampled_it_clearance_opti_down_trans")
     samples_place_dir = opj(base_dir, "test", "sampled_place_exec")
 
     follow_up_file = opj(base_dir,'test', 'follow_up_process.csv')
@@ -57,23 +58,30 @@ if __name__ == '__main__':
         it_b0 = vedo.load(opj(samples_it_dir, scene_name, interaction, "body_0.ply" ))
         it_b1 = vedo.load(opj(samples_it_dir, scene_name, interaction, "body_1.ply" ))
         it_b2 = vedo.load(opj(samples_it_dir, scene_name, interaction, "body_2.ply" ))
+        it_b0_opti_down = vedo.load(opj(samples_it_optim_down_dir, scene_name, interaction, "body_0.ply" )).color("green")
+        it_b1_opti_down = vedo.load(opj(samples_it_optim_down_dir, scene_name, interaction, "body_1.ply" )).color("green")
+        it_b2_opti_down = vedo.load(opj(samples_it_optim_down_dir, scene_name, interaction, "body_2.ply" )).color("green")
 
-        plt = vedo.Plotter(shape=(3,4), title=f"{dataset_name}/{scene_name}", size=(1024,1080), axes=0)
 
-        plt.show(vedo_scene+place_b0_orig, at=0)
-        plt.show(vedo_scene+place_b0_opt1, at=1)
-        plt.show(vedo_scene+place_b0_opt2, at=2)
-        plt.show(vedo_scene+it_b0, at=3)
+        plt = vedo.Plotter(shape=(3,5), title=f"{dataset_name}/{scene_name}", size=(1800,1000), axes=0)
 
-        plt.show(vedo_scene+place_b1_orig, at=4)
-        plt.show(vedo_scene+place_b1_opt1, at=5)
-        plt.show(vedo_scene+place_b1_opt2, at=6)
-        plt.show(vedo_scene+it_b1, at=7)
+        plt.show(vedo_scene+place_b0_orig, "PLACE, No optimization" ,at=0)
+        plt.show(vedo_scene+place_b0_opt1, "PLACE SimOptim", at=1)
+        plt.show(vedo_scene+place_b0_opt2, "PLACE AdvOptim", at=2)
+        plt.show(vedo_scene+it_b0,"iTCleareance", at=3)
+        plt.show(vedo_scene+it_b0_opti_down,"iTCleareance OptiDown", at=4)
 
-        plt.show(vedo_scene+place_b2_orig, at=8)
-        plt.show(vedo_scene+place_b2_opt1, at=9)
-        plt.show(vedo_scene+place_b2_opt2, at=10)
-        plt.show(vedo_scene+it_b2, at=11 )
+        plt.show(vedo_scene+place_b1_orig, at=5)
+        plt.show(vedo_scene+place_b1_opt1, at=6)
+        plt.show(vedo_scene+place_b1_opt2, at=7)
+        plt.show(vedo_scene+it_b1, at=8)
+        plt.show(vedo_scene+it_b1_opti_down, at=9)
+
+        plt.show(vedo_scene+place_b2_orig, at=10)
+        plt.show(vedo_scene+place_b2_opt1, at=11)
+        plt.show(vedo_scene+place_b2_opt2, at=12)
+        plt.show(vedo_scene+it_b2, at=13 )
+        plt.show(vedo_scene+it_b2_opti_down, at=14 )
         vedo.interactive()
 
         plt.close()
