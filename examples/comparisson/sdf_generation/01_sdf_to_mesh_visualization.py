5146d3282e2f7dcc245a649a158d3b137351348b
Abel Pacheco Ortega <dougbel.unam@gmail.com>
2021-07-01 14:13:36 +0100

    Enhance: visualization of  sdf and its data

--- examples/comparisson/sdf_generation/01_sdf_to_mesh_visualization.py
+++ examples/comparisson/sdf_generation/01_sdf_to_mesh_visualization.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     grid_dim = 256
-    dataset_name = "mp3d"
+    dataset_name = "prox"
 
     scenes_dir = f"/media/dougbel/Tezcatlipoca/PLACE_trainings/datasets/{dataset_name}/scenes"
 
@@ -28,34 +28,40 @@ if __name__ == "__main__":
 
     for scene_file in os.listdir(scenes_dir): # ['MPH1Library.ply']:
 
+        # A) load mesh
         scene_name = scene_file[:scene_file.find(".ply")]
-
         mesh_orig = trimesh.load(os.path.join(scenes_dir, scene_file))
         vedo_orig = vedo.load(os.path.join(scenes_dir, scene_file))
 
-        sdf = np.load(os.path.join(sdf_dir, scene_name+"_sdf.npy")).reshape(grid_dim, grid_dim, grid_dim)
-        vertices, faces, normals, _ = measure.marching_cubes(sdf, level=0)
-        mesh_reconstructed = trimesh.Trimesh(vertices=vertices, faces=faces, vertex_normals=normals)
+        # B) load json data
+        with open(os.path.join(sdf_dir, scene_name + ".json")) as f:
+            test_data = json.load(f)
+        print(f"{scene_name} differences json and mesh")
+        print(f"diff MAXs: {test_data['max'] - mesh_orig.bounding_box.vertices.max(axis=0)} ")
+        print(f"diff MINs: {test_data['min'] - mesh_orig.bounding_box.vertices.min(axis=0)} ")
+        orig_ext = mesh_orig.bounding_box.extents
+        json_ext = test_data['max'] - np.array(test_data['min'])
+        print(f"extends orig: {orig_ext} ")
+        print(f"extends json: {json_ext} ")
+        print(f"proportions: {json_ext[0] / orig_ext[0]}, {json_ext[1] / orig_ext[1]}, {json_ext[2] / orig_ext[2]} ")
 
-        if os.path.exists(os.path.join(sdf_dir, scene_name+".json")):
-            with open(os.path.join(sdf_dir, scene_name+".json")) as f:
-                test_data = json.load(f)
-            print(f"{scene_name} differences json and mesh")
-            print(f"diff MAXs: {test_data['max']-mesh_orig.bounding_box.vertices.max(axis=0)} ")
-            print(f"diff MINs: {test_data['min']-mesh_orig.bounding_box.vertices.min(axis=0)} ")
-            orig_ext=mesh_orig.bounding_box.extents
-            json_ext=test_data['max'] - np.array(test_data['min'])
-            print(f"extends orig: {orig_ext} ")
-            print(f"extends json: {json_ext} ")
-            print(f"proportions: {json_ext[0]/orig_ext[0]}, {json_ext[1]/orig_ext[1]}, {json_ext[2]/orig_ext[2]} ")
-            print("---------------------------------------")
+        # C ) bounding box and min max marked in original mesh
+
+        vedo_pmin = vedo.Point(test_data["min"], c="red")
+        vedo_pmax = vedo.Point(test_data["max"], c="red")
 
         vp = vedo.Plotter(verbose=0, title=scene_name, bg="white", size=(1200, 800), axes=8)
         vedo_orig.backFaceCulling(True)
         vp.add(vedo_orig)
+        vp.add((vedo_pmax))
+        vp.add((vedo_pmin))
         vp.show()
         vp.close()
 
+
+        sdf = np.load(os.path.join(sdf_dir, scene_name + "_sdf.npy")).reshape(grid_dim, grid_dim, grid_dim)
+        vertices, faces, normals, _ = measure.marching_cubes(sdf, level=0)
+        mesh_reconstructed = trimesh.Trimesh(vertices=vertices, faces=faces, vertex_normals=normals)
 
 
         vp = vedo.Plotter(verbose=0,title="Corrected SDF "+scene_name,  bg="white", size=(1200, 800), axes=8)
@@ -63,6 +69,6 @@ if __name__ == "__main__":
         vedo_reconstructed.c("green")
         vedo_reconstructed.backFaceCulling(True)
         vp.add(vedo_reconstructed)
-        vp.add(vedo_orig)
+        # vp.add(vedo_orig)
         vp.show()
         vp.close()
