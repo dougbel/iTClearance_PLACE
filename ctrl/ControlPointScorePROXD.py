2503d7693593dc9dff2f5e39e011f092fc64ec70
Abel Pacheco Ortega <dougbel.unam@gmail.com>
2021-08-12 14:45:39 +0100

    Added: function for optimise itClearance outputs

--- ctrl/ControlPointScorePROXD.py
+++ ctrl/ControlPointScorePROXD.py
@@ -29,7 +29,7 @@ from si.fulldataclearancescores import FullDataClearanceScores
 from util.util_mesh import find_files_mesh_env, read_sdf
 from util.util_preprocessing import crop_scene_cube_smplx_at_point, crop_scene_sphere_smplx_at_point
 from util.util_proxd import load_smplx_model, load_vposer_model, translate_smplx_body, rotate_smplx_body, \
-    get_vertices_from_body_params, get_trimesh_from_body_params, device
+    get_vertices_from_body_params, get_trimesh_from_body_params, device, optimize_body_on_environment
 from utils import get_contact_id, convert_to_6D_rot, convert_to_3D_rot, gen_body_mesh
 from view.ViewPointScorePROXD import ViewPointScorePROXD
 
@@ -208,171 +208,36 @@ class ControlPointScorePROXD():
 
         return np_nearest_point, best_angle
 
-
     def optimize_best_scored_position(self, np_point, best_angle, view_evolution_screens=False):
 
-        np_body_params = rotate_smplx_body(self.np_body_params, self.smplx_model, best_angle)
-        np_body_params = translate_smplx_body(np_body_params, self.smplx_model, np_point)
-
-        body_trimesh_proxd = get_trimesh_from_body_params(self.smplx_model, self.vposer_model, np_body_params)
-
         if self.view.started:
-            body_trimesh_proxd.visual.face_colors = [150, 150, 0, 255]
-            body_vedo_proxd = trimesh2vtk(body_trimesh_proxd)
-            self.view.add_vedo_element(body_vedo_proxd, at=1)
+            vtk_object = load(self.tester.it_descriptor.object_filename())
+            vtk_object.c([150, 150, 0])
+            vtk_object.rotate(best_angle, axis=(0, 0, 1), rad=True)
+            vtk_object.pos(x=np_point[0], y=np_point[1], z=np_point[2])
 
-        selected_p = body_trimesh_proxd.bounding_sphere.centroid
-
-        print('[INFO] Position selected.')
-
-        weight_loss_rec_verts = 1.0
-        weight_loss_rec_bps = 1.0
-        weight_loss_vposer = 0.02
-        weight_loss_shape = 0.01
-        weight_loss_hand = 0.01
-        weight_collision = 8.0
-        weight_loss_contact = 0.5
-        itr_s2 = 150
+            self.view.add_vedo_element(vtk_object, at=1)
 
         scene_trimesh = trimesh.load(self.file_mesh_env)
-        scene_verts = scene_trimesh.vertices
+        contact_ids, _ = get_contact_id(body_segments_folder=opj(self.prox_dataset_dir, 'body_segments'),
+                                        contact_body_parts=self.contact_regions)
 
-        cube_size = body_trimesh_proxd.bounding_sphere.extents[0] * 1.2
-        r = cube_size/2
-        scene_verts_local, scene_verts_crop_local, shift = crop_scene_sphere_smplx_at_point(scene_verts, selected_p, r=r)
-
-        print('[INFO] scene mesh cropped and shifted.')
-
-        scene_basis_set = bps_gen_ball_inside(n_bps=10000, random_seed=100)
-
-        scene_verts_crop_global = scene_verts_crop_local / cube_size
-
-        scene_bps, selected_scene_verts_global, selected_ind = bps_encode_scene(scene_basis_set,
-                                                                                scene_verts_crop_global)  # [n_feat, n_bps]
-        selected_scene_verts_local = scene_verts_crop_local[selected_ind]
-
-        print('[INFO] bps encoding computed.')
-
-        # position body params in the new reference point "shift"
-        shifted_rotated_scene = trimesh.Trimesh(vertices=scene_verts_local, faces=scene_trimesh.faces)
-        shifted_rotated_scene.visual.face_colors = scene_trimesh.visual.face_colors
-
-        body_params = translate_smplx_body(np_body_params, self.smplx_model, shift)
-
-        if view_evolution_screens:
-            shifted_rotated_body_smplx_trimesh = get_trimesh_from_body_params(self.smplx_model, self.vposer_model, body_params)
-            s = trimesh.Scene()
-            s.add_geometry(shifted_rotated_scene)
-            s.add_geometry(shifted_rotated_body_smplx_trimesh)
-            s.add_geometry(trimesh.points.PointCloud(selected_scene_verts_local, colors=[0, 255, 0]))
-            s.show(caption="smplx_model shifted and rotated", flags={'axis': True})
-
-        # extract body params of body positioned at "shift" and scaled to cube size
-        np_body_verts_sample = get_vertices_from_body_params(self.smplx_model, self.vposer_model, body_params) / cube_size
-        body_verts_sample = torch.from_numpy(np_body_verts_sample).float().unsqueeze(0).to(device).permute(0, 2, 1)
-
-        nbrs = NearestNeighbors(n_neighbors=1, leaf_size=1, algorithm="ball_tree").fit(np_body_verts_sample)
-        np_body_bps_sample, neigh_ind = nbrs.kneighbors(selected_scene_verts_global)
-        body_bps_sample = torch.from_numpy(np_body_bps_sample).float().unsqueeze(0).unsqueeze(0).to(device)
-
-        if view_evolution_screens:
-            body_trimesh_sampled = trimesh.Trimesh(np_body_verts_sample * cube_size, faces=self.smplx_model.faces)
-            body_trimesh_sampled.visual.face_colors = [0, 255, 255, 100]
-            s = trimesh.Scene()
-            s.add_geometry(shifted_rotated_scene)
-            s.add_geometry(body_trimesh_sampled)
-            s.show(caption="body sampled (verifying scale)", flags={'axis': True})
-
-        vid, _ = get_contact_id(body_segments_folder=opj(self.prox_dataset_dir, 'body_segments'),
-                                contact_body_parts=self.contact_regions)
-
-        # initialize smplx params, bs=1, local 3D cage coordinate system
-        body_params_rec = torch.from_numpy(body_params).float().unsqueeze(0).to(device)
-        body_params_rec = convert_to_6D_rot(body_params_rec)
-        body_params_rec.requires_grad = True
-
-
-        print('[INFO] start optimization ...')
-        optimizer = optim.Adam([body_params_rec], lr=0.01)
-
-        body_verts = body_verts_sample.permute(0, 2, 1)  # [1, 10475, 3]
-        body_verts = body_verts * cube_size  # to local 3d cage coordinate system scale
-
-        for step in tqdm(range(itr_s2)):
-            optimizer.zero_grad()
-
-            body_params_rec_72 = convert_to_3D_rot(body_params_rec)  # tensor, [bs=1, 72]
-            body_pose_joint = self.vposer_model.decode(body_params_rec_72[:, 16:48], output_type='aa').view(1, -1)
-            body_verts_rec = gen_body_mesh(body_params_rec_72, body_pose_joint, self.smplx_model)[0]  # [n_body_vert, 3]
-
-            # transform body verts to unit ball global coordinate
-            body_verts_rec_global = body_verts_rec / cube_size  # scale into unit ball
-
-            # calculate body_bps_rec
-            body_bps_rec = torch.zeros(body_bps_sample.shape)
-            if weight_loss_rec_bps > 0:
-                nbrs = NearestNeighbors(n_neighbors=1, leaf_size=1, algorithm="ball_tree").fit(
-                    body_verts_rec_global.detach().cpu().numpy())
-                neigh_dist, neigh_ind = nbrs.kneighbors(selected_scene_verts_global)
-                body_bps_rec = body_verts_rec_global[neigh_ind.squeeze()] - torch.from_numpy(
-                    selected_scene_verts_global).float().to(device)  # [n_bps, 3]
-                body_bps_rec = torch.sqrt(
-                    body_bps_rec[:, 0] ** 2 + body_bps_rec[:, 1] ** 2 + body_bps_rec[:, 2] ** 2).unsqueeze(0).unsqueeze(
-                    0)  # [bs=1, 1, n_bps]
-
-            ### body bps encoding reconstruct loss
-            loss_rec_verts = F.l1_loss(body_verts_rec.unsqueeze(0), body_verts)
-            loss_rec_bps = F.l1_loss(body_bps_sample, body_bps_rec)
-
-            ### vposer loss
-            body_params_rec_72 = convert_to_3D_rot(body_params_rec)
-            vposer_pose = body_params_rec_72[:, 16:48]
-            loss_vposer = torch.mean(vposer_pose ** 2)
-            ### shape prior loss
-            shape_params = body_params_rec_72[:, 6:16]
-            loss_shape = torch.mean(shape_params ** 2)
-            ### hand pose prior loss
-            hand_params = body_params_rec_72[:, 48:]
-            loss_hand = torch.mean(hand_params ** 2)
-
-            # transfrom body_verts_rec (local 3d cage coordinate system) to prox coordinate system
-            temp = body_verts_rec - torch.from_numpy(shift).float().to(device)
-            body_verts_rec_prox = temp.unsqueeze(0)  # tensor, [bs=1, 10475, 3]
-
-            ### sdf collision loss
-            norm_verts_batch = (body_verts_rec_prox - self.s_grid_min_batch) / (self.s_grid_max_batch - self.s_grid_min_batch) * 2 - 1
-            n_verts = norm_verts_batch.shape[1]
-            body_sdf_batch = F.grid_sample(self.s_sdf_batch.unsqueeze(1),
-                                           norm_verts_batch[:, :, [2, 1, 0]].view(-1, n_verts, 1, 1, 3),
-                                           padding_mode='border')
-            # if there are no penetrating vertices then set sdf_penetration_loss = 0
-            if body_sdf_batch.lt(0).sum().item() < 1:
-                loss_collision = torch.tensor(0.0, dtype=torch.float32).to(device)
-            else:
-                loss_collision = body_sdf_batch[body_sdf_batch < 0].abs().mean()
-
-            ### contact loss
-            body_verts_contact = body_verts_rec.unsqueeze(0)[:, vid, :]  # [1,1121,3]
-            dist_chamfer_contact = ext.chamferDist()
-            # scene_verts: [bs=1, n_scene_verts, 3]
-            scene_verts = torch.from_numpy(scene_verts_local).float().to(device).unsqueeze(0)  # [1,50000,3]
-            contact_dist, _ = dist_chamfer_contact(body_verts_contact.contiguous(), scene_verts.contiguous())
-            loss_contact = torch.mean(torch.sqrt(contact_dist + 1e-4) / (torch.sqrt(contact_dist + 1e-4) + 1.0))
-
-
-            loss = weight_loss_rec_verts * loss_rec_verts + weight_loss_rec_bps * loss_rec_bps + weight_loss_vposer * loss_vposer + weight_loss_shape * loss_shape + weight_loss_hand * loss_hand + weight_collision * loss_collision + weight_loss_contact * loss_contact
-            loss.backward(retain_graph=True)
-            optimizer.step()
-
-        print('[INFO] optimization stage 2 finished.')
-
-        body_params_opt_s2 = convert_to_3D_rot(body_params_rec)  # tensor, [bs=1, 72]
-        np_body_params_optim = translate_smplx_body(body_params_opt_s2.detach().squeeze().cpu().numpy(), self.smplx_model, -shift)
-        body_trimesh_optim = get_trimesh_from_body_params(self.smplx_model, self.vposer_model, np_body_params_optim)
-        body_trimesh_optim.visual.face_colors = [255, 255, 255, 255]
-        body_vedo_optim = trimesh2vtk(body_trimesh_optim)
+        body_trimesh_optim, np_body_params_optim = optimize_body_on_environment(
+                                        scene_trimesh, self.s_grid_min_batch, self.s_grid_max_batch, self.s_sdf_batch,
+                                        self.smplx_model, self.vposer_model,
+                                        self.np_body_params, np_point, best_angle, contact_ids,
+                                        weight_loss_rec_verts=1.0,
+                                        weight_loss_rec_bps=1.0,
+                                        weight_loss_vposer=0.02,
+                                        weight_loss_shape=0.01,
+                                        weight_loss_hand=0.01,
+                                        weight_collision=8.0,
+                                        weight_loss_contact=0.5,
+                                        itr_s2=150,
+                                        view_evolution_screens=view_evolution_screens)
 
         if self.view.started:
+            body_vedo_optim = trimesh2vtk(body_trimesh_optim)
             self.view.add_vedo_element(body_vedo_optim, at=1)
 
         return body_trimesh_optim, np_body_params_optim
---
+++ examples/comparisson/testing/04_itC_optimizer_SMPLX_body_parameters.py
@@ -0,0 +1,133 @@
+import gc
+import json
+import os
+import random
+from os.path import join as opj
+from shutil import copyfile
+
+import pandas as pd
+import numpy as np
+import trimesh
+
+from ctrl.ControlPointScorePROXD import ControlPointScorePROXD
+from util.util_mesh import read_sdf
+from util.util_proxd import optimize_body_on_environment, load_smplx_model, load_vposer_model
+from utils import get_contact_id
+
+if __name__ == '__main__':
+
+    visualize = True
+    shuffle_order = False  # if shuffle is True then execution would be SLOWER
+    save_results = False
+
+    base_dir = "/media/dougbel/Tezcatlipoca/PLACE_trainings"
+
+    datasets_dir = opj(base_dir, "datasets")
+
+    descriptors_dir = opj(base_dir, "config", "descriptors_repository")
+
+    smplx_model_dir = opj(base_dir, "pretrained_place", "body_models", "smpl")
+    vposer_model_dir = opj(base_dir, "pretrained_place", "body_models", "vposer_v1_0")
+
+    it_results_dir = opj(base_dir, 'test', 'env_test')
+    samples_dir = opj(base_dir, 'test', 'sampled_it_clearance')
+    output_dir = opj(base_dir, 'test', 'sampled_it_clearance_opti_smplx')
+
+    follow_up_file = opj(base_dir, 'test', 'follow_up_process.csv')
+    previus_follow_up_column = "it_auto_samples"
+    current_follow_up_column = "it_auto_samples_opti_smplx"
+
+    follow_up_data = pd.read_csv(follow_up_file, index_col=[0, 1, 2])
+    if current_follow_up_column not in follow_up_data.columns:
+        follow_up_data[current_follow_up_column] = False
+
+    num_total_task = follow_up_data.index.size
+    pending_tasks = list(follow_up_data[(follow_up_data[current_follow_up_column] == False)
+                                        & (follow_up_data[previus_follow_up_column] == True)].index)
+    num_pending_tasks = len(pending_tasks)
+    num_completed_task = follow_up_data[follow_up_data[current_follow_up_column] == True].index.size
+
+    print('STARTING TASKS: total %d, done %d, pendings %d' % (num_total_task, num_completed_task, num_pending_tasks))
+
+    if shuffle_order:
+        random.shuffle(pending_tasks)
+
+    vposer_model = load_vposer_model(vposer_model_dir)
+
+    last_env = None
+    last_gender = None
+    last_interaction = None
+
+    for dataset, env_name, interaction in pending_tasks:
+        print(dataset, env_name, interaction)
+
+        if last_env != env_name:
+            # get enviroment
+            env_trimesh = trimesh.load(opj(datasets_dir, dataset, "scenes", f"{env_name}.ply"))
+            # get sdf environment information
+            s_grid_min_batch, s_grid_max_batch, s_sdf_batch = read_sdf(opj(datasets_dir, dataset), env_name)
+            last_env = env_name
+
+
+        if last_interaction != interaction:
+            #get body "gender" and "contact regions"
+            json_descriptor_file = [f for f in os.listdir(opj(descriptors_dir, interaction)) if f.endswith(".json")][0]
+            with open( opj(descriptors_dir, interaction, json_descriptor_file) ) as jsonfile:
+                descriptor_data = json.load(jsonfile)
+            contact_regions = descriptor_data["extra"]["contact_regions"]
+            body_gender = descriptor_data["extra"]["body_gender"]
+
+            #get body params
+            body_params_file = [f for f in os.listdir(opj(descriptors_dir, interaction)) if f.endswith("_smplx_body_params.npy")][0]
+            np_body_params = np.load(opj(descriptors_dir, interaction,body_params_file))
+
+            last_interaction = interaction
+
+
+        if last_gender != body_gender:
+            smplx_model = load_smplx_model(smplx_model_dir, body_gender)
+            last_gender = body_gender
+
+        for i in range(follow_up_data.loc[dataset, env_name, interaction]['num_it_auto_samples']):
+
+            np_point = np.load(opj(samples_dir,env_name, interaction, f"point_{i}.npy"))
+            np_best_angle = np.load(opj(samples_dir,env_name, interaction, f"angle_{i}.npy"))
+
+            contact_ids, _ = get_contact_id(body_segments_folder=opj(datasets_dir, "prox", 'body_segments'),
+                                            contact_body_parts=contact_regions)
+
+            body_trimesh_optim, np_body_params_optim = optimize_body_on_environment(
+                env_trimesh, s_grid_min_batch, s_grid_max_batch, s_sdf_batch,
+                smplx_model, vposer_model,
+                np_body_params, np_point, np_best_angle, contact_ids,
+                weight_loss_rec_verts=1.0,
+                weight_loss_rec_bps=1.0,
+                weight_loss_vposer=0.02,
+                weight_loss_shape=0.01,
+                weight_loss_hand=0.01,
+                weight_collision=8.0,
+                weight_loss_contact=0.5,
+                itr_s2=150,
+                view_evolution_screens=visualize)
+
+            if visualize:
+                s = trimesh.Scene()
+                s.add_geometry(env_trimesh)
+                s.add_geometry(body_trimesh_optim)
+                s.show()
+
+            if save_results:
+                output_subdir = opj(output_dir, env_name, interaction)
+                if not os.path.exists(output_subdir):
+                    os.makedirs(output_subdir)
+                body_trimesh_optim.export(opj(output_subdir, f"body_{i}.ply"))
+
+        if save_results:
+            num_completed_task += 1
+            num_pending_tasks -= 1
+            copyfile(follow_up_file, follow_up_file + "_backup")
+            follow_up_data.at[(dataset, env_name, interaction), current_follow_up_column] = True
+            follow_up_data.to_csv(follow_up_file)
+            print(f"UPDATE: total {num_total_task}, done {num_completed_task}, pendings {num_pending_tasks}")
+
+        gc.collect()
