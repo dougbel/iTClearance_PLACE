d261ae5dfd1a5c3d3d66582b2f0bc5f5087abe5c
Abel Pacheco Ortega <dougbel.unam@gmail.com>
2021-08-17 23:34:37 +0100

    Added:PLACE execution with parameters as in demo from github

---
+++ scripts/01_task_iT_PLACE_place[demo_conf].sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export HDF5_USE_FILE_LOCKING='FALSE'
+
+#conda activate keras_gpu
+export PYTHONPATH=/home/alexa/Abel/projects/04_place_execution/iTClearance_PLACE:/home/alexa/Abel/DATA/git_repositories/iTpy:/home/alexa/Abel/DATA/git_repositories/iTpyClearance:/home/alexa/Abel/DATA/git_repositories/PLACE:/home/alexa/Abel/DATA/git_repositories/smplx:\$PYTHONPATH
+
+
+DEVICE=1
+
+
+
+
+CUDA_VISIBLE_DEVICES=$DEVICE python iTClearance_PLACE/examples/comparisson/testing/03_test_place_AUTO_selected_points[demo_conf].py  --base_dir /home/alexa/Abel/DATA/PLACE_trainings
+
