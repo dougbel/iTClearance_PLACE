0819a1c2c8318659a3be3012509f1a890b34092f
Abel Pacheco Ortega <dougbel.unam@gmail.com>
2021-12-15 17:23:30 +0000

    Added: scripts used during ablation study

---
+++ examples/comparisson/testing/ablation_study/scripts/measure_scores_per_interaction_type_ICP_1_calculation_alexa_gpu0.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export HDF5_USE_FILE_LOCKING='FALSE'
+
+#conda activate keras_gpu
+export PYTHONPATH=/home/alexa/Abel/projects/04_place_execution/iTClearance_PLACE:/home/alexa/Abel/DATA/git_repositories/iTpy:/home/alexa/Abel/DATA/git_repositories/iTpyClearance:/home/alexa/Abel/DATA/git_repositories/PLACE:/home/alexa/Abel/DATA/git_repositories/smplx:\$PYTHONPATH
+
+DEVICE=0
+
+CUDA_VISIBLE_DEVICES=$DEVICE python /home/alexa/Abel/projects/04_place_execution/iTClearance_PLACE/examples/comparisson/testing/ablation_study/03_measure_scores_per_interaction_type_ICP_1_calculation.py
+
+CUDA_VISIBLE_DEVICES=$DEVICE python /home/alexa/Abel/projects/04_place_execution/iTClearance_PLACE/examples/comparisson/testing/ablation_study/03_measure_scores_per_interaction_type_ICP_1_calculation.py
+
+
